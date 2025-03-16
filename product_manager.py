f3437b8b1df091e2d6f2eb733c6b5068d022ed3f
Poki <boyan.bk@gmail.com>
2025-03-16 19:00:12 +0100

    U product manager dodat deo koda za uklanjanje proizvoda

--- product_manager.py
+++ product_manager.py
@@ -16,3 +16,11 @@ class ProductManager:
     def total_inventory_value(self):
         total_value = sum(product.price * product.quantity for product in self.products)
         print(f"Total inventory value: {total_value}")
+
+def remove_product(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                self.products.remove(product)
+                print(f"Product {product_name} removed.")
+                return
+        print(f"Product {product_name} not found.")
